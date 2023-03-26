163da5c5a1a9752d8e7d8d90410d29acf1ea0d9c
adriano-ramoss <adrianoramos2401@gmail.com>
2023-03-26 19:37:40 -0300

    Aula: Método - Exercício

--- src/ExerciciosPOO/Carros.java
+++ src/ExerciciosPOO/Carros.java
@@ -2,7 +2,43 @@ package ExerciciosPOO;
 
 public class Carros {
 	String cor = "";
-	String Modelo = "";
-	int CapacidadeTanque = 0;
+	String modelo = "";
+	int capacidadeTanque = 0;
+
+	Carros() {
+
+	}
+
+	Carros(String cor, String modelo, int capacidadeTanque) { //Construtor e sbrecarga da classe Carros
+		this.cor = cor; //parametros
+		this.modelo = modelo; //This é uma forma de diferenciar oq pertence e não pertence ao objeto
+		this.capacidadeTanque = capacidadeTanque;
+	}
+
+	void setCor(String cor) { //Guarda o valor de Cor
+		this.cor = cor;
+
+	}
+
+	String getCor() { //Devolve o valor de Cor guardado no set
+		return cor;
+	}
+
+	void setModelo(String modelo) {  //Guarda o valor de Modelo guardado no set
+		this.modelo = modelo;
+
+	}
+	String getModelo() { //Devolve o valor de Modelo guardado no set
+		return modelo;
+	}
+	void setCapacidadeTanque(int capacidadeTanque) { //Guarda o valor de CapacidadeTanque
+		this.capacidadeTanque = capacidadeTanque;
+	}
+	int getCapacidadeTanque() { //Devolve o valor de CapacidadeTanque guardado no set
+		return capacidadeTanque;
+	}
+	double TotalValorTanque() {
+		return capacidadeTanque = capacidadeTanque * ValorCombustivel;
+	}
 
 }
